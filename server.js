02b4964e9fd3155c496842cd1ebb16d699e32b5e
giqbal <gohars.iqbal@gmail.com>
2018-07-18 16:20:22 +0100

    Retrieved html contents of requested URL

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http')
 const {crawlUrl} = require('./controller/index')
 
 const server = http.createServer((req, res) => {
-  if (req.url.includes('/api/crawl/')) crawlUrl(req, res)
+  if (req.url.includes('/api/crawl')) crawlUrl(req, res)
 })
 
 module.exports = server
